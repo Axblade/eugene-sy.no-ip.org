d1ef9a838c7fcd7c1dde049ac6c3d725fb95c409
Axblade <eugene.sypachev@gmail.com>
2013-07-03 19:29:48 +0400

    moved config files

--- lib/config.rb
+++ lib/config.rb
@@ -1,7 +1,7 @@
 require 'digest/md5'
 require 'yaml'
 
-config = YAML.load_file("config.yaml")
+config = YAML.load_file("config/config.yaml")
 email_address = config["config"]["email"]
 hash = Digest::MD5.hexdigest(email_address)
 size = config["config"]["size"]
